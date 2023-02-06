314ab51ea09fdf04a39a5fb70d7895c5f10b93ff
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-02-06 16:15:09 -0500

    Updated GraphicsRenderer to draw segments
    Updated the visualizer to draw the segements with their color
    attributes. Individually goes through the x and y segments seperately.

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -2,6 +2,7 @@ package ca.mcmaster.cas.se2aa4.a2.visualizer;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 
 import java.awt.Graphics2D;
@@ -9,6 +10,7 @@ import java.awt.Stroke;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 import java.util.List;
 
 public class GraphicRenderer {
@@ -18,7 +20,15 @@ public class GraphicRenderer {
         canvas.setColor(Color.BLACK);
         Stroke stroke = new BasicStroke(0.5f);
         canvas.setStroke(stroke);
-        for (Vertex v: aMesh.getVerticesList()) {
+
+        List<Vertex> vertex_list=aMesh.getVerticesList();
+
+        List<Segment> segmentx=aMesh.getSegmentsList().subList(0,600);
+        List<Segment> segmenty=aMesh.getSegmentsList().subList(600,1200);
+
+
+        for (int i=0, j=0, k=0; i< vertex_list.size(); i++){
+            Vertex v=vertex_list.get(i);
             double centre_x = v.getX() - (THICKNESS/2.0d);
             double centre_y = v.getY() - (THICKNESS/2.0d);
             Color old = canvas.getColor();
@@ -26,7 +36,46 @@ public class GraphicRenderer {
             Ellipse2D point = new Ellipse2D.Double(centre_x, centre_y, THICKNESS, THICKNESS);
             canvas.fill(point);
             canvas.setColor(old);
+
         }
+
+        int x=0;
+        int y=0;
+        for (Segment s: segmentx){
+            Color old=canvas.getColor();
+            canvas.setColor(extractColor(s.getPropertiesList()));
+            Line2D line=new Line2D.Double(s.getV1Idx(), y, s.getV2Idx(), y);
+            canvas.draw(line);
+            canvas.setColor(old);
+
+            if (y==480){
+                x+=20;
+                y=0;
+            }else{
+                y+=20;
+            }
+
+        }
+
+        x=0;
+        y=0;
+        for (Segment s: segmenty){
+            Color old=canvas.getColor();
+            canvas.setColor(extractColor(s.getPropertiesList()));
+            Line2D line=new Line2D.Double(x, s.getV1Idx(), x, s.getV2Idx());
+            canvas.draw(line);
+            canvas.setColor(old);
+
+            if (x==480){
+                y+=20;
+                x=0;
+            }else{
+                x+=20;
+            }
+        }
+
+
+
     }
 
     private Color extractColor(List<Property> properties) {
@@ -47,3 +96,5 @@ public class GraphicRenderer {
     }
 
 }
+
+
