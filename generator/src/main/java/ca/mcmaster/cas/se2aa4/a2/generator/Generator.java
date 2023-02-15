b77973022972d401ede4d7016f763c8e8be4b38f
Sathurshan <arulmohs@mcmaster.ca>
2023-02-15 14:52:35 -0500

    Create a Coordinate List that has the vertex in Coordinate form

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Generator.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Generator.java
@@ -2,6 +2,10 @@ package ca.mcmaster.cas.se2aa4.a2.generator;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.locationtech.jts.geom.Coordinate;
+
 public class Generator {
 
     private final int squareSize = 20;
@@ -10,6 +14,11 @@ public class Generator {
 
         //RegularMesh mesh = new RegularMesh(width, height, 2, squareSize);
         IrregularMesh mesh = new IrregularMesh(width, height, 2, numberPolygons);
+
+        List<Coordinate> ya= mesh.getCoordinates();
+        for (Coordinate j: ya){
+            System.out.println(j.getX() + " "  + j.getY());
+        }
 
         return Mesh.newBuilder().addAllPolygons(mesh.getMesh()).addAllSegments(mesh.getSegments()).addAllVertices(mesh.getCentroids()).addAllVertices(mesh.getVertices()).build();
 
