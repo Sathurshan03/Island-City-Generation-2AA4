19a9ab82df8e334d55ff22b9122e339e390fa2c9
Sathurshan <arulmohs@mcmaster.ca>
2023-02-15 15:51:20 -0500

    Give shorter command line argument options and added new command line option of relaxation level without any implmentation

--- generator/src/main/java/Main.java
+++ generator/src/main/java/Main.java
@@ -9,9 +9,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
         Options options = new Options();
-        options.addOption("numPoly", true, "Number of Polygons");
-        options.addOption("width", true, "Width of Canvas");
-        options.addOption("height", true, "Height of Canvas");
+        options.addOption(new Option("np", "numPoly", true, "Number of Polygons"));
+        options.addOption(new Option("wth", "width", true, "Width of Canvas"));
+        options.addOption(new Option("hth", "height", true, "Height of Canvas"));
+        options.addOption(new Option("rl", "relation", true, "Relaxation level for Llyod relaxation"));
         options.addOption(new Option("h", "help", false, "Available Options and Command Line Arguments"));
         
         
@@ -19,12 +20,14 @@ public class Main {
         int numberPolygons = 0;
         int canvasWidth = 500;
         int canvasHeight = 500;
+        int relationLevel = 0;
         
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
         String numPoly = cmd.getOptionValue("numPoly");
         String width = cmd.getOptionValue("width");
         String height = cmd.getOptionValue("height");
+        String relation = cmd.getOptionValue("relation");
 
         if(cmd.hasOption("-help")) {
             //ADD MORE INSTRUCTIONS HERE TO EXPLAIN HOW TO USE COMMAND LINE ARGUMENTS
@@ -42,7 +45,16 @@ public class Main {
         if(height != null){
             canvasHeight = Integer.parseInt(height);
         }
+        if(relation != null){
+            relationLevel = Integer.parseInt(relation);
+        }
         
+        System.out.println(numberPolygons);
+        System.out.println(canvasWidth);
+        System.out.println(canvasHeight);
+        System.out.println(relationLevel);
+
+
         Generator generator = new Generator();
         Mesh myMesh = generator.generate(numberPolygons, canvasWidth, canvasHeight);
         MeshFactory factory = new MeshFactory();
