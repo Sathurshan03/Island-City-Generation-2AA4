100234bc9005845a0774ab366243792b36576fc9
Sathurshan <arulmohs@mcmaster.ca>
2023-02-15 17:13:19 -0500

    Implemented Feature F19: choose grid or irregular mesh generation from command line

--- generator/src/main/java/Main.java
+++ generator/src/main/java/Main.java
@@ -2,6 +2,7 @@ import ca.mcmaster.cas.se2aa4.a2.generator.Generator;
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import org.apache.commons.cli.*;
+import ca.mcmaster.cas.se2aa4.a2.generator.MeshType;
 
 import java.io.IOException;
 
@@ -9,6 +10,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
         Options options = new Options();
+        options.addOption(new Option("g", "grid", false, "Generate Grid Mesh"));
+        options.addOption(new Option("i", "irregular", false, "Generate Irregular Mesh"));
         options.addOption(new Option("np", "numPoly", true, "Number of Polygons"));
         options.addOption(new Option("wth", "width", true, "Width of Canvas"));
         options.addOption(new Option("hth", "height", true, "Height of Canvas"));
@@ -28,12 +31,27 @@ public class Main {
         String width = cmd.getOptionValue("width");
         String height = cmd.getOptionValue("height");
         String relation = cmd.getOptionValue("relation");
+        MeshType meshType;
 
         if(cmd.hasOption("-help")) {
             //ADD MORE INSTRUCTIONS HERE TO EXPLAIN HOW TO USE COMMAND LINE ARGUMENTS
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("help", options);
             return;
+        }
+        
+        //checks if valid input for generating grid or irregular mesh
+        if(cmd.hasOption("grid") && cmd.hasOption("irregular")) {
+            throw new IOException("Can not generate both grid and irregular mesh");
+        }
+        else if (cmd.hasOption("grid")) {
+            meshType = MeshType.GRID;
+        }
+        else if (cmd.hasOption("irregular")) {
+            meshType = MeshType.IRREGULAR;
+        }
+        else{
+            throw new IOException("Need to generate either grid or irregular mesh");
         }
 
         if(numPoly != null){
@@ -49,14 +67,11 @@ public class Main {
             relationLevel = Integer.parseInt(relation);
         }
         
-        System.out.println(numberPolygons);
-        System.out.println(canvasWidth);
-        System.out.println(canvasHeight);
-        System.out.println(relationLevel);
 
+        //NEED TO STILL USE relationLevel
 
         Generator generator = new Generator();
-        Mesh myMesh = generator.generate(numberPolygons, canvasWidth, canvasHeight);
+        Mesh myMesh = generator.generate(numberPolygons, canvasWidth, canvasHeight, meshType);
         MeshFactory factory = new MeshFactory();
         factory.write(myMesh, args[0]);
     }
--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Generator.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Generator.java
@@ -2,7 +2,7 @@ package ca.mcmaster.cas.se2aa4.a2.generator;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 import org.locationtech.jts.geom.Coordinate;
 
@@ -10,17 +10,20 @@ public class Generator {
 
     private final int squareSize = 20;
 
-    public Mesh generate(int numberPolygons, int width, int height) {
+    public Mesh generate(int numberPolygons, int width, int height, MeshType meshType) throws IOException{
 
-        //RegularMesh mesh = new RegularMesh(width, height, 2, squareSize);
-        IrregularMesh mesh = new IrregularMesh(width, height, 2, numberPolygons);
-
-        List<Coordinate> ya= mesh.getCoordinates();
-        for (Coordinate j: ya){
-            System.out.println(j.getX() + " "  + j.getY());
+        if (meshType.equals(MeshType.GRID)){
+            RegularMesh mesh = new RegularMesh(width, height, 2, squareSize); 
+            return Mesh.newBuilder().addAllPolygons(mesh.getMesh()).addAllSegments(mesh.getSegments()).addAllVertices(mesh.getCentroids()).addAllVertices(mesh.getVertices()).build();
         }
-
-        return Mesh.newBuilder().addAllPolygons(mesh.getMesh()).addAllSegments(mesh.getSegments()).addAllVertices(mesh.getCentroids()).addAllVertices(mesh.getVertices()).build();
+        else if (meshType.equals(MeshType.IRREGULAR)){
+            IrregularMesh mesh = new IrregularMesh(width, height, 2, numberPolygons);
+            List<Coordinate> ya= mesh.getCoordinates();
+            return Mesh.newBuilder().addAllPolygons(mesh.getMesh()).addAllSegments(mesh.getSegments()).addAllVertices(mesh.getCentroids()).addAllVertices(mesh.getVertices()).build();
+        }
+        else{
+            throw new IOException("No type of mesh determine. Can not generate a mesh");
+        }
 
     }
 
