e079031fcfa26422ba9fe25a5e77a0af4f8ae8a4
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-02-15 20:35:33 -0500

    Updated IrregularMesh to use GeoStruct
    Updated the IrregularMesh to store and facilitate the process of using
    CustomPolygons, CustomVertex and CustomSegments. It creates the
    centroids, and generates the lines in between using CustomPolygon.

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/IrregularMesh.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/IrregularMesh.java
@@ -1,7 +1,13 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.triangulate.VoronoiDiagramBuilder;
+
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class IrregularMesh extends MeshADT {
@@ -9,15 +15,23 @@ public class IrregularMesh extends MeshADT {
 
     public IrregularMesh(int width, int height, int precision){
         super(width,height, precision);
+        collection_centroid =new ArrayList<>();
         centroids=new ArrayList<>();
         vertices=new ArrayList<>();
         segments=new ArrayList<>();
         createCentroids();
 
-        for (int i=0; i<centroids.size(); i++){
-            CustomPolygon polygon=new CustomPolygon(i, precision);
-            addPolygon(polygon.gePolygon());
+        GeometryFactory geometryFactory = new GeometryFactory();
+        VoronoiDiagramBuilder voronoiDiagramBuilder = new VoronoiDiagramBuilder();
+        voronoiDiagramBuilder.setSites(collection_centroid);
+        List<Polygon> polygons = voronoiDiagramBuilder.getSubdivision().getVoronoiCellPolygons(geometryFactory);
+
+
+        for (Polygon i:polygons){
+            GeoStruct conversion=new GeoStruct(i);
+            addPolygon(conversion.getCusPolygon().gePolygon());
         }
+
 
 
     }
@@ -32,6 +46,7 @@ public class IrregularMesh extends MeshADT {
             CustomVertex new_centroid=new CustomVertex(random_x,random_y,new Color(254,0,0,254), "2.0", precision);
 
             centroids.add(new_centroid);
+            collection_centroid.add(new Coordinate(random_x,random_y));
 
         }
 
