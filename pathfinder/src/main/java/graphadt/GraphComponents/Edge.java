6ccc4e9bb9801c3a95531736de490090f89aadd8
Sathurshan <arulmohs@mcmaster.ca>
2023-03-29 19:15:23 -0400

    Calculate the shortest path between two given nodes using Dijkstra's algorithm

--- pathfinder/src/main/java/graphadt/GraphComponents/Edge.java
+++ pathfinder/src/main/java/graphadt/GraphComponents/Edge.java
@@ -5,10 +5,10 @@ public class Edge {
     private Node node1;
     private Node node2;
 
-    public Edge(double weight, Node node1, Node node2){
-        this.weight = weight;
+    public Edge(Node node1, Node node2, double weight){
         this.node1 = node1;
         this.node2 = node2;
+        this.weight = weight;
     }
 
     public double getWeight(){
@@ -21,4 +21,27 @@ public class Edge {
         }
         return false;
     }
+
+    public Node getNode1(){
+        return node1;
+    }
+
+    public Node getNode2(){
+        return node2;
+    }
+
+    public boolean isNode1 (Node node){
+        if (node.equals(node1)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isNode2 (Node node){
+        if (node.equals(node2)){
+            return true;
+        }
+        return false;
+    }
+
 }
